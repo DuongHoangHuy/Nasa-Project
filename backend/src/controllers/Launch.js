b9cf2b9e70b9f1d7493197f66cf021430fd97a04
Dương Hoàng Huy <huydh@MacBook-Pro-cua-Duong.local>
2023-07-01 11:27:49 +0700

    update workflow

--- backend/src/controllers/Launch.js
+++ backend/src/controllers/Launch.js
@@ -6,6 +6,7 @@ const DEFAULT_PAGE_NUMBER = 1;
 const DEFAULT_PAGE_LIMIT = 0;
 
 const getPagination = (query) => {
+  console.log('query', query)
   const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
   const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
   const skip = (page - 1) * limit;
